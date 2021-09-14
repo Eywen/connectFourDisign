f62a07606382cb7bc2630a6bbbeb88b5bb24c755
Blanca <b.castano@almatech.es>
2021-09-14 14:01:31 +0200

    agregando codigo turno

--- src/connectfour/disign/Turn.java
+++ src/connectfour/disign/Turn.java
@@ -8,11 +8,24 @@ public class Turn {
     private int activePlayer;
 
     public Turn(Board board) {
+        assert null != board;
         this.board = board;
         players = new Player[NUMBER_PLAYERS];
     }
 
     public void play() {
-        players[activePlayer].play(board);
+        players[activePlayer].play();
+        //si no es fin de juego activo el sgt jugador
+       /* if (!this.board.isTicTacToe(this.getActiveColor())){
+            this.activePlayer = (this.activePlayer+1) % Turn.NUMBER_PLAYERS;
+        }*/
     }
+
+   /* void writeWinner(){
+        this.players[this.activePlayer].writeWinner();
+    }*/
+
+    /*Color getActiveColor() {
+        return this.players[this.activePlayer].getColor();
+    }*/
 }
